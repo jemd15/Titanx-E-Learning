c0b8148fb82451e549e62f59213bde5ef9ed68d9
Javier Muñoz <x_jemd_x@hotmail.com>
2020-05-12 20:05:48 -0400

    navbar, home y footer visualmente listos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,4 +12,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  scrollToElement($element): void {
+    console.log($element);
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
 }
