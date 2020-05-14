d7d3a3fdaf4d1a7d7a445e0b17ac3a8fa6ee9462
Javier Muñoz <x_jemd_x@hotmail.com>
2020-05-14 18:07:37 -0400

    btn de smooth scroll desaparece en el top de la página

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +9,26 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  scrollTo(element: string) {
+    document.querySelector(element).scrollIntoView({
+      behavior: 'smooth'
+    });
   }
 
-  scrollTo(element: string){
-    document.querySelector(element).scrollIntoView({ 
-      behavior: 'smooth' 
-    });
+  @HostListener("window:scroll", ['$event'])
+  showActionButton($event: Event) {
+    let actionButton = document.querySelector('.btn-floating');
+    let distanceOfTop = document.documentElement.scrollTop; // distancia desde el punto más alto hasta el scroll actual.
+    if (distanceOfTop > 690) {
+      actionButton.setAttribute('style', 'opacity: 1'); // aquí se agrega propiedad css quizas de una forma incorrecta, pero funcional.
+      actionButton.classList.remove("disabled"); // deshabilitamos el botón cuando no se muestra
+    } else {
+      actionButton.setAttribute('style', 'opacity: 0'); // aquí se agrega propiedad css quizas de una forma incorrecta, pero funcional.
+      actionButton.classList.add("disabled"); // volvemos a habilitar el botón cuando es visible
+    }
+
   }
 
 }
